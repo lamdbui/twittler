1d4fb8ec04619bb543bfc4b18559637050a37c92
Lam Bui <lamdbui@gmail.com>
2017-08-04 17:50:17 +0100

    Fix implementation for new tweet and cleanup

--- application.js
+++ application.js
@@ -1,23 +1,17 @@
-function refreshCurrentTweets(node, tweets) {
+window.visitor;
+
+function refreshCurrentTweets() {
+  let $node = $('.feed');
+  let tweets = streams.home;
   for (let i = 0; i < tweets.length; i++) {
     var tweet = tweets[i];
     var $tweet = $('<div></div>');
     $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.prependTo(node);
+    $tweet.prependTo($node);
   }
 }
 
-function createNewTweet(user, message) {
-  let tweet = {};
-  tweet.user = user;
-  tweet.message = message;
-  tweet.date = new Date();
-  addTweet(tweet);
-  refreshCurrentTweets($feed, streams.home);
-}
-
 $(document).ready(function(){
-  //let $body = $('body');
   let $feed = $('.feed');
 
   // configure our initial page state
@@ -25,32 +19,30 @@ $(document).ready(function(){
 
   // set click handlers
   $('.refresh').on('click', function() {
-    refreshCurrentTweets($feed, streams.home);
+    refreshCurrentTweets();
   });
   $('.new_tweet').on('click', function() {
     $('.new_tweet_form').slideToggle();
   });
-  $('.new_tweet_form').submit(function() {
-    // let user = $('#username').val();
-    // let message = $('#message').val();
-    // let user = document.findElementsByName('username');
-    // let message = document.findElementsByName('message');
+  $('.new_tweet_form').submit(function(event) {
+    // NOTE: this is important to prevent default form submission handling
+    event.preventDefault();
+
     let inputData =  $('.new_tweet_form :input').serializeArray();
-    // TODO: should we do some validation here?
-    createNewTweet(inputData[0].value, inputData[1].value);
+    visitor = inputData[0].value;
+    let message = inputData[1].value;
+
+    if (visitor.length && message.length) {
+      // create a new user, if there isn't one already
+      if (!streams.users.hasOwnProperty(visitor)) {
+        streams.users[visitor] = [];
+      }
+      writeTweet(message);
+    }
     setTimeout(function() {
       $('.new_tweet_form').slideUp();
     }, 1000);
   });
-  // $('.new_tweet_submit').on('click', function() {
-  //   //TODO: Add a slick notification message
-  //   let user = $()
-  //   createNewTweet
-  //   setTimeout(function() {
-  //     $('.new_tweet_ui').slideUp();
-  //   }, 1000);
-  // });
 
-  refreshCurrentTweets($feed, streams.home);
-
+  refreshCurrentTweets();
 });
