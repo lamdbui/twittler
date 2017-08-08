0aabe68ed17fe6d24c038a02e9d0c332e9267920
Lam Bui <lamdbui@gmail.com>
2017-08-08 16:18:14 -0700

    Make additional layout adjustments with fonts

--- application.js
+++ application.js
@@ -2,20 +2,37 @@ window.visitor;
 
 var currentTweetCount = 0;
 
+var refreshTweetTimeElapsed = function() {
+  let tweets = streams.home;
+  for (let i = 0; i < tweets.length; i++) {
+    let timeDifferenceStr = moment(tweet.created_at).fromNow();
+    //let $tweetDate = $('<p class="date"></p>');
+    tweets[i].created_at()
+  }
+}
+
+var refreshTweetTime = function(dateNode) {
+  let timeDifferenceStr = moment(tweet.created_at).fromNow();
+  dateNode.text(' - '.concat(timeDifferenceStr));
+}
+
 var refreshCurrentTweets = function() {
   let $node = $('.feed');
   let tweets = streams.home;
-  for (let i = 0; i < tweets.length; i++) {
-    var tweet = tweets[i];
-    var $tweet = $('<div class="tweet"></div>');
-    var $tweetUsername = $('<a class="username" href="#">@HLS</a>');
-    //var $tweetDate = $('<')
-    var $tweetMessage = $('<p>MESSAGE</p>');
-    //$tweet.text('@' + tweet.user + ': ' + tweet.message);
+  for (let i = currentTweetCount; i < tweets.length; i++) {
+    let tweet = tweets[i];
+    let timeDifferenceStr = moment(tweet.created_at).fromNow();
+    let $tweet = $('<div class="tweet"></div>');
+    let $tweetUsername = $('<a class="username" href="#">@HLS</a>');
+    let $tweetDate = $('<p class="date"></p>');
+    let $tweetMessage = $('<p class="message"></p>');
+
     $tweetUsername.text('@' + tweet.user);
+    $tweetDate.text(' - '.concat(timeDifferenceStr));
     $tweetMessage.text(tweet.message);
     $tweet.prependTo($node);
     $tweetUsername.appendTo($tweet);
+    $tweetDate.appendTo($tweet);
     $tweetMessage.appendTo($tweet);
   }
   $('.tweet:odd').css('background-color', '#DDD');
@@ -31,11 +48,6 @@ var checkForNewTweets = function(timeInterval) {
 
   $('.feed_status').text('Click to update ' + numOfNewTweets + ' new tweets');
 
-  // if (numOfNewTweets === 0) {
-  //   $('.feed_status').slideUp();
-  // } else {
-  //   $('.feed_status').slideDown();
-  // }
   if (numOfNewTweets > 0) {
     $('.feed_status').slideDown();
   }
@@ -65,6 +77,8 @@ $(document).ready(function(){
     // NOTE: this is important to prevent default form submission handling
     event.preventDefault();
 
+    currentTweetCount = streams.home.length;
+
     let inputData =  $('#new_tweet_form :input').serializeArray();
     visitor = inputData[0].value;
     let message = inputData[1].value;
@@ -76,11 +90,10 @@ $(document).ready(function(){
       }
       writeTweet(message);
     }
-    setTimeout(function() {
-      $('#new_tweet_form').slideUp();
-    }, 1000);
-
-    currentTweetCount = stream.home.length;
+    $('#new_tweet_form').slideUp();
+    // setTimeout(function() {
+    //   $('#new_tweet_form').slideUp();
+    // }, 1000);
   });
 
   refreshCurrentTweets();
